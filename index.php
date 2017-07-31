68794aa931c8b79b33cd2972485a0328217da1c3
mittrayash <mittrayash@gmail.com>
2017-08-01 03:14:00 +0530

    Dental Website for Facial Virtuoso init

---
+++ index.php
@@ -0,0 +1,40 @@
+<!--Strat Header-->
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset="utf-8">
+    <title>Dental Website</title>
+    <meta name="description" content="We deal only in Dentistry and We are Expert at it">
+
+
+    <!--[if IE]> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <![endif]-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Google Fonts -->
+    <link rel="stylesheet" href="css/main.css">
+    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700%7COpen+Sans:300,300i,400,400i,600,600i,700,800" rel="stylesheet">
+    <link rel="stylesheet" href="http://www.facialvirtuoso.com/assets/css/plugins.min.css">
+    <link rel="stylesheet" href="http://www.facialvirtuoso.com/assets/css/settings.css">
+    <link rel="stylesheet" href="http://www.facialvirtuoso.com/assets/css/layers.css">
+    <link rel="stylesheet" href="http://www.facialvirtuoso.com/assets/css/navigation.css">
+    <link rel="stylesheet" href="http://www.facialvirtuoso.com/assets/css/style.css">
+    <link rel="stylesheet" href="http://www.facialvirtuoso.com/assets/css/mvs.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="http://www.facialvirtuoso.com/admin_assets/css/custom.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script
+    <style type="text/css">
+
+    </style>
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="images/logo2.png">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.3.54/theme-default.min.css">
+
+    <!-- Modernizr -->
+    <script src="http://www.facialvirtuoso.com/assets/js/modernizr.js"></script>
