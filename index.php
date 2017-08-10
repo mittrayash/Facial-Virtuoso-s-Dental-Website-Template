c257d6c81a1b60e1122996adbac131b41867fc95
mittrayash <mittrayash@gmail.com>
2017-08-11 04:14:00 +0530

    Completed scroller

--- index.php
+++ index.php
@@ -131,9 +131,36 @@
         </ol>
 
 
+		<!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox" style="height: 450px; !important;">
+            <div class="item active"> <img  src="images/dental1.jpg" alt="banner3">
+                <div class="carousel-caption"> </div>
+            </div>
+            <div class="item"> <img  src="images/dental2.jpg" alt="banner1" >
+                <div class="carousel-caption"> </div>
+            </div>
+            <!-- <div class="item"> <img  src="assetsimages/myimages/banner3.jpg" alt="banner3">
+              <div class="carousel-caption"> </div>
+            </div> -->
+            <div class="item">
+                <img  src="images/dental3.jpg" alt="banner2">
+                <div class="carousel-caption"> </div>
+            </div>
+        </div>
 
+<!-- Controls -->
+        <a class="left carousel-control" href="#carousel-example-generic" style="top:300px;" role="button" data-slide="prev"> <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span> <span class="sr-only">Previous</span> </a> <a class="right carousel-control" href="#carousel-example-generic" style="top:300px;" role="button" data-slide="next"> <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> <span class="sr-only">Next</span> </a> </div>
+    <!--End Slider-->
+    <div id="into" class="into-slider" id="myScroll">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-3 ontop odd hvr-pulse-grow" style="">
+                    <div class="item">
+                        <center><span class="glyphicon glyphicon-send"></span></center><br>
+                        <h4><center>3D Printed Models</center></h4>
 
-
+                    </div>
+                </div>
 
 
 
