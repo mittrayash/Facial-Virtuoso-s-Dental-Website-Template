358198b1d102b960716b9d2fa99e1668985672ae
mittrayash <mittrayash@gmail.com>
2017-08-24 09:14:00 +0530

    Added 3d Models section

--- index.php
+++ index.php
@@ -446,6 +446,23 @@ We can help you execute your plan exactly, accurately and safely with our innova
                                     <figure>
                                         <img src="images/1.jpg" class="memberImg"  alt="Member Name">
                                     </figure>
+				</div>
+                                <div class="col-sm-12" style="background-color: darkslategray;">
+                                    <a href="#"><h3 class="member-title" style="margin-top: 10px;  color: bisque;"><center>3D PRINTED MODEL FOR SURGICAL PLANING</center></h3></a>
+                                </div>
+
+                                <div class="col-sm-12" style="background-color: darkslategray; text-align: center; padding-bottom:5%; font-size: 17px;"><hr> <span class="text-center" style="color: white; text-align: justify">
+    3D printed models are great tools to explain the disease to the patient and the planned surgery.It also helps to plan the case better.</span>
+                                </div>
+                            </div>
+
+                            <div class="member col-sm-10">
+                                <div>
+                                    <figure>
+                                        <img src="images/3.jpg" class="memberImg"   alt="Member Name">
+                                    </figure>
+
+
 									
 <script type="text/javascript" async="async" defer="defer" data-cfasync="false" src="https://mylivechat.com/chatinline.aspx?hccid=18484045"></script>
 <!-- <script>
