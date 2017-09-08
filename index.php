5ea1e02c0b76e09f42a082f493916c9b6c58d7ab
mittrayash <mittrayash@gmail.com>
2017-09-08 12:14:00 +0530

    jquery for slider added

--- index.php
+++ index.php
@@ -919,6 +919,89 @@ We can help you execute your plan exactly, accurately and safely with our innova
     }
 </style>
 <?php require_once('footer.php');?>
+
+</div>
+</div><!-- End #wrapper -->
+
+<a id="scroll-top" href="#top" title="Scroll top"><i class="fa fa-angle-up"></i></a>
+
+<!-- End -->
+<script src="http://www.facialvirtuoso.com/assets/js/plugins.min.js"></script>
+<script src="http://www.facialvirtuoso.com/assets/js/main.js"></script>
+<script type="text/javascript" src="http://www.facialvirtuoso.com/assets/js/mvs.js"></script>
+
+<script type="text/javascript">
+    jQuery(document).ready(function() {
+        "use strict";
+
+        var revapi;
+        if ( $("#rev_slider").revolution == undefined ) {
+            revslider_showDoubleJqueryError("#rev_slider");
+        } else {
+            revapi = $("#rev_slider").show().revolution({
+                sliderType: "standard",
+                jsFileLocation: "assets/js/",
+                sliderLayout: "auto",
+                dottedOverlay:"none",
+                delay: 15000,
+                navigation: {
+                    mouseScrollNavigation: "off",
+                    onHoverStop: "off",
+                    touch: {
+                        touchenabled: "on"
+                    },
+                    arrows: {
+                        style: "hebe",
+                        enable: true,
+                        hide_onmobile: false,
+                        hide_onleave: false,
+                        hide_under: 992,
+                        tmp: '',
+                        left: {
+                            h_align: "left",
+                            v_align: "center",
+                            h_offset: 0,
+                            v_offset: 0
+                        },
+                        right: {
+                            h_align: "right",
+                            v_align: "center",
+                            h_offset: 0,
+                            v_offset: 0
+                        }
+                    },
+                    bullets: {
+                        enable: true,
+                        hide_onmobile: false,
+                        style: "hades",
+                        hide_onleave: false,
+                        hide_under: 768,
+                        direction: "horizontal",
+                        h_align: "center",
+                        v_align: "bottom",
+                        h_offset: 0,
+                        v_offset: 25,
+                        space: 8,
+                        tmp: '<span class="tp-bullet-image"></span>'
+                    }
+                },
+                responsiveLevels: [1200,992,768,480],
+                gridwidth: [1170,970,750,480],
+                gridheight: [600,520,460,360],
+                lazyType: "smart",
+                spinner: "spinner3",
+                parallax: {
+                    type: "mouse",
+                    origo: "slidercenter",
+                    speed: 2000,
+                    levels: [2,3,4,5,6,7,12,16,10,50],
+                    disable_onmobile: "on"
+                },
+                debugMode: false
+            });
+        }
+    });
+</script>
 						
 <script type="text/javascript" async="async" defer="defer" data-cfasync="false" src="https://mylivechat.com/chatinline.aspx?hccid=18484045"></script>
 <!-- <script>
